df25e5f480d0f55ed4b69cc0d723d09a749afab3
Grace Glenn <mgglenn@g.clemson.edu>
2017-02-21 11:32:10 -0500

    added --filename option and cleaned up csv formatting

--- Exporter.py
+++ Exporter.py
@@ -1,40 +1,38 @@
-# -*- coding: utf-8 -*-
-
-import sys,getopt,got,datetime,codecs
+import sys
+import getopt
+import got
+import datetime
+import codecs
+import csv
 
 def main(argv):
-
 	if len(argv) == 0:
 		print 'You must pass some parameters. Use \"-h\" to help.'
 		return
 		
-	if len(argv) == 1 and argv[0] == '-h':
-		print """\nTo use this jar, you can pass the folowing attributes:
-    username: Username of a specific twitter account (without @)
-       since: The lower bound date (yyyy-mm-aa)
-       until: The upper bound date (yyyy-mm-aa)
- querysearch: A query text to be matched
-   maxtweets: The maximum number of tweets to retrieve
+        filename = "my_tweets.csv"
+        tweet_rows = []
 
- \nExamples:
- # Example 1 - Get tweets by username [barackobama]
- python Exporter.py --username "barackobama" --maxtweets 1\n
-
- # Example 2 - Get tweets by query search [europe refugees]
- python Exporter.py --querysearch "europe refugees" --maxtweets 1\n
-
- # Example 3 - Get tweets by username and bound dates [barackobama, '2015-09-10', '2015-09-12']
- python Exporter.py --username "barackobama" --since 2015-09-10 --until 2015-09-12 --maxtweets 1\n
- 
- # Example 4 - Get the last 10 top tweets by username
- python Exporter.py --username "barackobama" --maxtweets 10 --toptweets\n"""
-		return
- 
 	try:
-		opts, args = getopt.getopt(argv, "", ("username=", "since=", "until=", "querysearch=", "toptweets", "maxtweets="))
+		opts, args = getopt.getopt(argv, "", ("filename=", "username=", "since=", "until=", "querysearch=", "toptweets", "maxtweets="))
 		
 		tweetCriteria = got.manager.TweetCriteria()
-		
+
+                tweet_header = []
+
+                tweet_header.append("username")
+                tweet_header.append("date")
+                tweet_header.append("retweets")
+                tweet_header.append("favorites")
+                tweet_header.append("text")
+                tweet_header.append("geo")
+                tweet_header.append("mentions")
+                tweet_header.append("hashtags")
+                tweet_header.append("id")
+                tweet_header.append("permalink")
+        
+                tweet_rows.append(tweet_header)		
+
 		for opt,arg in opts:
 			if opt == '--username':
 				tweetCriteria.username = arg
@@ -51,29 +49,47 @@ def main(argv):
 			elif opt == '--toptweets':
 				tweetCriteria.topTweets = True
 				
+                        elif opt == '--filename':
+                            filename = arg
+
 			elif opt == '--maxtweets':
 				tweetCriteria.maxTweets = int(arg)
-				
-		
-		outputFile = codecs.open("output_got.csv", "w+", "utf-8")
-		
-		outputFile.write('username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink')
 		
 		print 'Searching...\n'
 		
 		def receiveBuffer(tweets):
 			for t in tweets:
-				outputFile.write(('\n%s;%s;%d;%d;"%s";%s;%s;%s;"%s";%s' % (t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites, t.text, t.geo, t.mentions, t.hashtags, t.id, t.permalink)))
-			outputFile.flush();
-			print 'More %d saved on file...\n' % len(tweets)
+
+                            current_tweet = []
+                            
+                            current_tweet.append(t.username)
+                            current_tweet.append(t.date.strftime(("%Y-%m-%d %H:%M")))
+                            current_tweet.append(t.retweets)
+                            current_tweet.append(t.favorites) 
+                            
+                            t.text = ''.join([i if ord(i) < 128 else ' ' for i in t.text])
+
+                            current_tweet.append(t.text)
+                            current_tweet.append(t.geo)
+                            current_tweet.append(t.mentions)
+                            current_tweet.append(t.hashtags)
+                            current_tweet.append(t.id)
+                            current_tweet.append(t.permalink)
+                            tweet_rows.append(current_tweet)
+
+			print '%d tweet(s) saved on file...\n' % len(tweets)
 		
 		got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
 		
 	except arg:
-		print 'Arguments parser error, try -h' + arg
+		print 'Arguments parser error, try -h'
 	finally:
-		outputFile.close()
-		print 'Done. Output file generated "output_got.csv".'
+                with open(filename, "wb") as f :
+                    fileWriter = csv.writer(f, dialect='excel')
+                    fileWriter.writerows(tweet_rows)
+                    f.close()
+
+		    print 'Done. Tweets stored in ' + filename
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
